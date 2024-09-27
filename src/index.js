7e171bd1e056004f0965b754e2a77a8b48bb2b82
liaozhe <1538153125@qq.com>
2024-09-27 15:08:19 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import App from './App'
 import 'normalize.css'
 import './assets/css/index.less'
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Suspense fallback="loading...">
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </Suspense>
 )
