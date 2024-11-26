d082b8aeac4ee37b1e1a6f77617c44022b9bf8e7
liaozhe <1538153125@qq.com>
2024-11-26 11:50:59 +0800

    feat:引入antd

--- craco.config.js
+++ craco.config.js
@@ -1,21 +1,21 @@
 const path = require('path')
-const CracoLessPlugin = require('craco-less');
+const CracoLessPlugin = require('craco-less')
 
 const resolve = pathname => path.resolve(__dirname, pathname)
 
 module.exports = {
-  // less
-  plugins: [
-    {
-      plugin: CracoLessPlugin
-    },
-  ],
-  // webpack
-  webpack: {
-    alias: {
-      "@": resolve("src"),
-      "components": resolve("src/components"),
-      "utils": resolve("src/utils")
-    }
-  }
+	// less
+	plugins: [
+		{
+			plugin: CracoLessPlugin
+		}
+	],
+	// webpack
+	webpack: {
+		alias: {
+			'@': resolve('src'),
+			components: resolve('src/components'),
+			utils: resolve('src/utils')
+		}
+	}
 }
