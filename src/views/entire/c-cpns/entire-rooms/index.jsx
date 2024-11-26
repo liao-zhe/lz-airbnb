25dc6416e4d7ef9a1146e470cc41e07feda36674
liaozhe <1538153125@qq.com>
2024-11-26 15:50:19 +0800

    feat:使用useCallBack优化性能

--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,9 +1,13 @@
-import React, { memo } from 'react'
+import React, { memo, useCallback } from 'react'
 import { EntireRoomsWrapper } from './style'
 import RoomItem from '@/components/room-item'
-import { shallowEqual, useSelector } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { changeDetailInfoActon } from '@/store/modules/detail'
 
 const EntireRooms = memo(props => {
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
 	const { roomList, isLoading, totalCount } = useSelector(
 		state => ({
 			roomList: state.entire.roomList,
@@ -13,12 +17,20 @@ const EntireRooms = memo(props => {
 		shallowEqual
 	)
 
+	const itemClickHandle = useCallback(
+		item => {
+			navigate('/detail')
+			dispatch(changeDetailInfoActon(item))
+		},
+		[navigate, dispatch]
+	)
+
 	return (
 		<EntireRoomsWrapper>
 			<div className="title">共{totalCount}多处住所</div>
 			<div className="list">
 				{roomList.map(item => {
-					return <RoomItem key={item._id} itemData={item} $itemwidth="20%" />
+					return <RoomItem key={item._id} itemData={item} $itemwidth="20%" itemClick={e => itemClickHandle(item)} />
 				})}
 			</div>
 			{isLoading && <div className="loading">加载中...</div>}
